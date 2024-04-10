df15418f031a4318e1cbc2b3c0e040e07795ec6e
Anthony Harris <anthony@ah.id.au>
2024-04-10 13:14:51 +0100

    Provide specs for behaviour against known special case documents.

---
+++ FsHtmlKit.Spec/ExamplesSpec.fs
@@ -0,0 +1,22 @@
+﻿module FsHtmlKit.Spec.ExamplesSpec
+
+open FsCheck
+open FsCheck.Xunit
+
+[<Property>]
+let ``E1.Complete HTML document with DOCTYPE declaration: no content`` () =
+    let exampleText = System.IO.File.ReadAllText("examples/01.html")
+
+    let expected: string option = None
+    let actual: string option = FsHtmlKit.Html2PlainText.Std.tryExtractPlainText exampleText
+
+    actual = expected
+
+[<Property>]
+let ``E2.Complete HTML document with DOCTYPE declaration: minimal content`` () =
+    let exampleText = System.IO.File.ReadAllText("examples/02.html")
+
+    let expected: string option = Some <| $"E02{System.Environment.NewLine}"
+    let actual: string option = FsHtmlKit.Html2PlainText.Std.tryExtractPlainText exampleText
+
+    actual = expected |> Prop.label(actual |> Option.defaultValue "Extraction failed")
